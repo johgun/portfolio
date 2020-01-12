b0413f151e4c2f4c871a388c7f7c1ff6564a5199
Johan <jogu84@gmail.com>
2020-01-12 15:41:57 +0100

    Updates

---
+++ precache-manifest.a1ef0839278dadcf261e0609a12e1e16.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7f5d717ace6bb75868d496d8987879",
+    "url": "/porfolio/index.html"
+  },
+  {
+    "revision": "6be9f99ef09816436e67",
+    "url": "/porfolio/static/css/main.ed97a815.chunk.css"
+  },
+  {
+    "revision": "576079fc0704d445daaf",
+    "url": "/porfolio/static/js/2.ef4036f8.chunk.js"
+  },
+  {
+    "revision": "6be9f99ef09816436e67",
+    "url": "/porfolio/static/js/main.39661fbd.chunk.js"
+  },
+  {
+    "revision": "a7c6afeb12165e513427",
+    "url": "/porfolio/static/js/runtime-main.c205ffba.js"
+  },
+  {
+    "revision": "47e70f6ff021c610674288ca517294f4",
+    "url": "/porfolio/static/media/Inter-Regular.47e70f6f.woff2"
+  },
+  {
+    "revision": "65cb1d3aec330c9d04750612eccb0c17",
+    "url": "/porfolio/static/media/TiemposHeadline-Regular.65cb1d3a.woff2"
+  },
+  {
+    "revision": "88c42afe13bdd156b786add6afd940c9",
+    "url": "/porfolio/static/media/TiemposText-Regular.88c42afe.woff2"
+  },
+  {
+    "revision": "37610e335b5fe17da54fb72a5f10e483",
+    "url": "/porfolio/static/media/bolon-banner.37610e33.jpg"
+  },
+  {
+    "revision": "3d0ed8f66f89148d6fb43dd38253da86",
+    "url": "/porfolio/static/media/bolon-big-image.3d0ed8f6.jpg"
+  },
+  {
+    "revision": "e5c40fb125ee45693cb3c787fc20b330",
+    "url": "/porfolio/static/media/bolon-design-1.e5c40fb1.jpg"
+  },
+  {
+    "revision": "3108af1f57e5c39539d90a4fd10d1720",
+    "url": "/porfolio/static/media/bolon-design-2.3108af1f.jpg"
+  },
+  {
+    "revision": "ff501671243d93d7fabcb2ed45848b63",
+    "url": "/porfolio/static/media/bolon-design-3.ff501671.jpg"
+  },
+  {
+    "revision": "c1ec296f7b3a272ff9ac44983df4a7f5",
+    "url": "/porfolio/static/media/bolon-hub.c1ec296f.jpg"
+  },
+  {
+    "revision": "3bc9551f8c661c696962f15638d31a3d",
+    "url": "/porfolio/static/media/bolon-scope.3bc9551f.png"
+  },
+  {
+    "revision": "c7e617227d1f1c403a7de5926c2ebe5c",
+    "url": "/porfolio/static/media/ccs-banner.c7e61722.jpg"
+  },
+  {
+    "revision": "95d6033583e2a3bf0e47300ff9b00979",
+    "url": "/porfolio/static/media/ccs-big-image.95d60335.jpg"
+  },
+  {
+    "revision": "3e3dc3defce85a5cb28e6a559273a7fd",
+    "url": "/porfolio/static/media/ccs-design-concept.3e3dc3de.jpg"
+  },
+  {
+    "revision": "26befc6407ceff304fd13357577ec1ef",
+    "url": "/porfolio/static/media/ccs-home.26befc64.jpg"
+  },
+  {
+    "revision": "8ed48dd221eba2572eac30270e5b6662",
+    "url": "/porfolio/static/media/ccs-identity.8ed48dd2.jpg"
+  },
+  {
+    "revision": "32b591f0c1cf629e9eeff6a6a46d185a",
+    "url": "/porfolio/static/media/ccs-method.32b591f0.jpg"
+  },
+  {
+    "revision": "ec758472127968c726c01ae8587554ff",
+    "url": "/porfolio/static/media/ccs-objects.ec758472.jpg"
+  },
+  {
+    "revision": "43e65c7530211845eb657a3654679cee",
+    "url": "/porfolio/static/media/ccs-pres.43e65c75.jpg"
+  },
+  {
+    "revision": "9989ed0683440fd816dae597a9118c34",
+    "url": "/porfolio/static/media/close-icon.9989ed06.svg"
+  },
+  {
+    "revision": "74284890c32e75b7057913f8159f0f80",
+    "url": "/porfolio/static/media/essve-banner.74284890.jpg"
+  },
+  {
+    "revision": "caf9644a70e31ccf0bcce37d1f01ae2c",
+    "url": "/porfolio/static/media/essve-big-image.caf9644a.jpg"
+  },
+  {
+    "revision": "897e01f652c591acbcda286755339c4b",
+    "url": "/porfolio/static/media/essve-design.897e01f6.jpg"
+  },
+  {
+    "revision": "da7f1c33523b3c39f475c644136de5c4",
+    "url": "/porfolio/static/media/essve-icons.da7f1c33.jpg"
+  },
+  {
+    "revision": "559b947e3a7753dfa9933042d21e459e",
+    "url": "/porfolio/static/media/essve-loader.559b947e.svg"
+  },
+  {
+    "revision": "1169ec0270f6c5723bdcb43a88889816",
+    "url": "/porfolio/static/media/essve-mood@2x.1169ec02.jpg"
+  },
+  {
+    "revision": "5de47e299f9f6cdad4d5056c7b1f8d2b",
+    "url": "/porfolio/static/media/essve-onboarding.5de47e29.jpg"
+  },
+  {
+    "revision": "3cdd477de53850b7d6895f96b7677184",
+    "url": "/porfolio/static/media/essve-sitemap.3cdd477d.jpg"
+  },
+  {
+    "revision": "0226fc57101f490e1cbac875956e804a",
+    "url": "/porfolio/static/media/long-arrow-black.0226fc57.svg"
+  },
+  {
+    "revision": "2b4f79eb56d4b9a2ac1dfe4f483a3edb",
+    "url": "/porfolio/static/media/long-arrow.2b4f79eb.svg"
+  },
+  {
+    "revision": "4ed5c52544e789d4713be906bce53112",
+    "url": "/porfolio/static/media/tiny-arrow-down.4ed5c525.svg"
+  },
+  {
+    "revision": "e2944b57bce990a6634449ce63294422",
+    "url": "/porfolio/static/media/tiny-arrow-up.e2944b57.svg"
+  }
+]);
